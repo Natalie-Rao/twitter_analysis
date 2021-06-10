0c75af6552dfb9e49fb4b742f0316594849cccde
Yushu Rao <yr872@nyu.edu>
2021-06-09 22:59:24 -0700

    add query twitter followers

--- crawler.py
+++ crawler.py
@@ -1,25 +1,73 @@
 import connect;
-
+import configparser
 
 import twint
 
 
-tweets = []
+def queryFollowers(feed, followersCount):
+    c = twint.Config()
 
-c = twint.Config()
+    c.Username = feed
+    c.Limit = followersCount
+    c.Store_object = True
+
+    twint.run.Followers(c)
+    followers = twint.output.follows_list
+    print(followers)
 
 
-c.Username = 'noneprivacy'
-c.Limit = 20
-c.Store_object = True
+def queryFeedStatus(feed, timerange, twittercount):
+    return ""
 
-c.Store_object_tweets_list = tweets
 
-twint.run.Search(c)
+def crawler(feed, level, timerange, twittercount):
+    tweets = []
+    c = twint.Config()
 
-tweets2db = [];
-for t in tweets:
-    tweets2db.append([c.Username, t.datestamp + " " + t.timestamp, t.tweet])
 
-connect.insert(tweets2db);
+    c.Username = feed
+    c.Limit = 20
+    c.Store_object = True
+
+    c.Store_object_tweets_list = tweets
+
+    twint.run.Search(c)
+
+    tweets2db = [];
+    for t in tweets:
+        tweets2db.append([c.Username, t.datestamp + " " + t.timestamp, t.tweet])
+
+    connect.insert(tweets2db);
+
+
+# main entry
+
+config = configparser.ConfigParser()
+config.read('crawler.ini')
+
+crawler_feeds = config['feeds']['feeds']
+
+feeds = crawler_feeds.split(",")
+print(feeds);
+
+for feed in feeds:
+    if not feed:
+        continue
+    print(feed)
+    _level = config[feed]['level']
+    _timerange = config[feed]['timerange']
+    _twittercount = config[feed]['twittercount']
+    _followers = config[feed]['followers']
+    print('start to crawl :', 'feed=' + feed, 'level='+_level, 'timerange='+_timerange, 'twittercount='+_twittercount, 'followers=' + _followers)
+    queryFollowers(feed, _followers)
+
+
+
+
+
+
+
+
+
+
 
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import connect;
-connect.deleteall()
-array = [['test1','2021-05-15','happy'],['test2','2021-05-16','excited'],['test3','2021-04-15','stoked']]
-connect.insert(array);
+#connect.deleteall()
+#array = [['test1','2021-05-15','happy'],['test2','2021-05-16','excited'],['test3','2021-04-15','stoked']]
+#connect.insert(array);
 array2 = connect.read();
 for x in array2:
     print(x)
